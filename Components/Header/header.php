c0a562f32fae3e612e4ae9c36cbb7d0f04f9a9d5
J.A Venura Jayaasingha <142327713+VenuraSLiit@users.noreply.github.com>
2024-04-07 14:23:30 +0530

    Header added and rough detailing completed

--- Components/Header/header.php
+++ Components/Header/header.php
@@ -15,6 +15,33 @@
 
 </head>
 <body>
+<!--List section-->
+<nav>
+
+    <div class="nav__logo">
+        <img id="nav_site_ico" src="../../src/airplane.png" alt="login">
+        Wix-Air</div>
+
+    <!--Link section -->
+    <ul class="nav__links">
+        <li class="link"><a href="#">Home</a></li>
+        <li class="link"><a href="#">Packages</a></li>
+        <li class="link"><a href="#">My Bookings</a></li>
+        <li class="link"><a href="#">About Us</a></li>
+
+    </ul>
+    <!--Contact Us button-->
+    <button class="btn">Contact Us</button>
+
+    <!--Login icon Section-->
+    <div class="tooltip">
+        <a href="#"><img id="nav_login_ico" src="../../src/login.png" alt="login"></a>
+        <span class="tool_tip_text">login</span>
+    </div>
+
+
+
+</nav>
 
 <!--Link for Javascript-->
 <script src="header.js"></script>
---
+++ php/dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+
+//declaring the Variables
+$servername = "localhost";
+$username = "Venu";
+$password = "gayasha123@";
+$dbname = "login_database";
+
+// Create connection to database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
