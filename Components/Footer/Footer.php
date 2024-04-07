7605edcc456660422b7439c79bf144091289a3ae
J.A Venura Jayaasingha <142327713+VenuraSLiit@users.noreply.github.com>
2024-04-07 11:51:36 +0530

    Header And Footer Added

---
+++ Components/Footer/Footer.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!--Link for Cascading Style sheet(CSS)-->
+    <link rel="stylesheet" href="Footer.css">
+
+</head>
+<body>
+
+
+<!--Link for Javascript-->
+<script src="Footer.js"></script>
+</body>
+
+</html>
+
---
+++ Components/Header/header.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!--Link for Cascading Style sheet(CSS)-->
+    <link rel="stylesheet" href="header.css">
+
+</head>
+<body>
+
+<!--Link for Javascript-->
+<script src="header.js"></script>
+</body>
+
+</html>
