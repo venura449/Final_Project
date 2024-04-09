4e5589ed6eaf66f14b5fc22f7c4425f618192cb2
J.A Venura Jayaasingha <142327713+VenuraSLiit@users.noreply.github.com>
2024-04-09 11:37:57 +0530

    Footer section completed

---
+++ Components/Footer/Footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Footer.css">
+
+    <!--Link for Icons-->
+    <link
+        href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css"
+        rel="stylesheet"
+    />
+</head>
+
+<body>
+<footer class="footer">
+    <div class="section__container footer__container">
+        <div class="footer__col">
+            <h3>WIX-Air</h3>
+            <p>
+                Where Excellence Takes Flight. With a strong commitment to customer
+                satisfaction and a passion for air travel, Flivan Airlines offers
+                exceptional service and seamless journeys.
+            </p>
+            <p>
+                From friendly smiles to state-of-the-art aircraft, we connect the
+                world, ensuring safe, comfortable, and unforgettable experiences.
+            </p>
+        </div>
+        <div class="footer__col">
+            <h4>INFORMATION</h4>
+            <p>Home</p>
+            <p>About</p>
+            <p>Packages</p>
+            <p>Reservation</p>
+            <p>Destinations</p>
+        </div>
+        <div class="footer__col">
+            <h4>CONTACT</h4>
+            <p>Support</p>
+            <p>Feedback</p>
+            <p>FAQ</p>
+        </div>
+    </div>
+    <div class="section__container footer__bar">
+        <p>Copyright © 2024_MLB_01.02_11 . All rights reserved.</p>
+        <div class="socials">
+            <span><i class="ri-facebook-fill"></i></span>
+            <span><i class="ri-twitter-fill"></i></span>
+            <span><i class="ri-instagram-line"></i></span>
+            <span><i class="ri-youtube-fill"></i></span>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
