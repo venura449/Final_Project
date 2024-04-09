8972fbc7f65cca1dd9d934d8bc941879cd193db9
J.A Venura Jayaasingha <142327713+VenuraSLiit@users.noreply.github.com>
2024-04-09 10:12:42 +0530

    Footer Added

--- pages/About_us/About_us.php
+++ pages/About_us/About_us.php
@@ -1,1 +1,30 @@
 <?php
+
+// Include the HTML content
+require_once("../../Components/Header/header.php");
+
+// Include the CSS file
+echo '<style>';
+require_once("../../Components/Header/header.css");
+echo '</style>';
+
+// Include the JavaScript file
+echo '<script>';
+require_once("../../Components/Header/header.js");
+echo '</script>';
+require_once("../../Components/Footer/Footer.php");
+
+// Include the HTML content
+require_once("../../Components/Footer/Footer.php");
+
+// Include the CSS file
+echo '<style>';
+require_once("../../Components/Footer/Footer.css");
+echo '</style>';
+
+// Include the JavaScript file
+echo '<script>';
+require_once("../../Components/Footer/Footer.js");
+echo '</script>';
+
+
